91b990c50d33c8ae03bad164b928e9b2633501a7
RudneyC <127165811+RudneyC@users.noreply.github.com>
2023-03-13 22:18:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 $(document).ready(function() {
 
+  // Define o nome do arquivo CSV
   var csvFileName = "dados.csv";
 
+  // Faz a leitura do arquivo CSV
   $.get(csvFileName, function (data) {
+
+    // Divide o conteúdo do arquivo CSV em linhas
     var lines = data.split("\n");
-    var tableHead = "<tr>" + lines[0].split(",").map(header => `<th>${header}</th>`).join("") + "</tr>";
-    var tableRows = lines.slice(1).map(row => "<tr>" + row.split(",").map(cell => `<td>${cell}</td>`).join("") + "</tr>");
+
+    // Extrai os cabeçalhos da tabela
+    var tableHead = "<tr>";
+    var headers = lines[0].split(",");
+    for (var i = 0; i < headers.length; i++) {
+      tableHead += "<th>" + headers[i] + "</th>";
+    }
+    tableHead += "</tr>";
     $("#table-head").append(tableHead);
-    $("#table-body").append(tableRows.join(""));
+
+    // Extrai os dados da tabela
+    for (var i = 1; i < lines.length; i++) {
+      var tableRow = "<tr>";
+      var cells = lines[i].split(",");
+      for (var j = 0; j < cells.length; j++) {
+        tableRow += "<td>" + cells[j] + "</td>";
+      }
+      tableRow += "</tr>";
+      $("#table-body").append(tableRow);
+    }
+
+    // Aplica a funcionalidade DataTables à tabela
     $('#myTable').DataTable();
+
   });
 });
